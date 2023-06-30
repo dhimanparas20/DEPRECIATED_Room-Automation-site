c6482a8cfc0c49a763703bd13ae9dabb6fa69e01
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2023-06-30 17:52:00 +0530

    Update base.js

--- static/js/base.js
+++ static/js/base.js
@@ -141,7 +141,7 @@ function refreshTime() {
             }
       }
   )}
-  setInterval(getvalue, 1000); 
+  setInterval(getvalue, 500); 
 
   function isResponseEqual(response1, response2) {
     // Return true if responses are equal, false otherwise
